17dd05c758350b2eb55a9272b867acf2ea6c78f1
Candan Dozen <candan.dozen@cern.ch>
2015-08-28 21:53:07 +0200

    initial commit

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-for file in output/*.tif; do 
-#   echo $file
-    ./Analysis $file > "$file.txt"
+CURRENT_DIR=`pwd`
+OUTPUT="$CURRENT_DIR/output"
+CAPTURED="$CURRENT_DIR/captured"
+
+SUBTRACT_THRESHOLD=50
+SUBTRACT_BACKGROUND="$CURRENT_DIR/backgrounds/frame_58.tif"
+
+
+cd $CAPTURED
+for file in *.tif; do 
+    echo "processing $file..."
+    $CURRENT_DIR/Subtract $file $SUBTRACT_BACKGROUND $SUBTRACT_THRESHOLD "$OUTPUT/$file.subtracted.tif"
+    $CURRENT_DIR/Analysis "$OUTPUT/$file.subtracted.tif" > "$OUTPUT/$file.txt"
 done
