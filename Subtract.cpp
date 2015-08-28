5601b674610af586beb507ade1198dec6c0014b9
Candan Dozen <candan.dozen@cern.ch>
2015-08-28 18:42:46 +0200

    update Subtract.cpp

--- Subtract.cpp
+++ Subtract.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <cv.h>
 #include <highgui.h>
+#include <cstdlib>
 
 using namespace cv;
 using namespace std;
@@ -14,20 +15,26 @@ using namespace std;
 int main( int argc, char** argv )
 {
     // define variables for the following operation:
-    // original - background -> diff
-    Mat original, background, diff;
+    // original - background -> diff -> [selective picking process] -> out
+    Mat original, background, diff, out;
+    unsigned int i, j;
+    float distance, threshold;
 
-    if(argc != 4) {
+    if(argc != 5) {
 	      printf("Number of parameters must be 3.\n");
-	      printf("example usage: Substract beam.png background.png out.png\n");
+	      printf("example usage: Substract beam.png background.png (INT THRESHOLD) out.png\n");
 	      return -1;
 	  }
-	  
-	  // first input is original image
-	  original   = imread(argv[1], 1);
-	  
-	  // second parameter is background image
-	  background = imread(argv[2], 1);
+
+    // first input is original image
+    original   = imread(argv[1], 1);
+    // cvtColor(original , gray_original , CV_BGR2GRAY);
+
+    // second parameter is background image
+    background = imread(argv[2], 1);
+    // cvtColor(background , gray_background , CV_BGR2GRAY);
+
+    threshold  = atof(argv[3]);
 
     // check if images are loaded successfully
     if(!original.data || !background.data) {
@@ -35,11 +42,21 @@ int main( int argc, char** argv )
         return -1;
     }
 
-    // subtrack images
     diff = original - background;
 
+    out = Mat::zeros(diff.rows, diff.cols, CV_8UC3);
+
+    for(i = 0; i < original.rows; i++)
+      for(j = 0; j < original.cols; j++) {
+            Vec3b pix = diff.at<Vec3b>(i, j);
+            distance  =  sqrt(pix[0]*pix[0]+pix[1]*pix[1]+pix[2]*pix[2]);
+            if(distance > threshold) {
+                out.at<Vec3b>(i, j) = original.at<Vec3b>(i, j);
+            }
+        }
+
     // write the image
-    imwrite(argv[3], diff);
-    
+    imwrite(argv[4], out);
+
     return 0;
 }
